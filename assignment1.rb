99fca061927988307965153b418a697a5a57141a
Cristian Benitez <cristianbenitez@lewisu.edu>
2019-01-27 07:28:22 +0000

    added some comments

--- assignment1.rb
+++ assignment1.rb
@@ -1,3 +1,5 @@
+# program that reads in two values from the user, converts them to integers, computes the sum, difference and product of the two values and displays the results.
+
 puts "Enter first number";
 x=gets.chomp.to_i
 puts "Enter second number";
@@ -7,4 +9,4 @@ dif=x-y;
 pro=x*y;
 puts "Sum = "+sum.to_s;
 puts "Difference = "+dif.to_s;
-puts "Product = "+pro.to_s;
+puts "Product = "+pro.to_s;ruby
